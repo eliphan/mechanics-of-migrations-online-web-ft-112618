74f7d4e97c9c14dc66d5184ceaed68a5d7b74ce3
Elise Phan <aliceconnic@gmail.com>
2019-01-12 04:05:24 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,5 +8,11 @@ class CreateArtists < ActiveRecord::Migration
   def change
   end
   
-  
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+
+
 end
