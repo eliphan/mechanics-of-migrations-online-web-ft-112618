5b88d9a0733ff0d36373ffa578dbc7b77f4c7d8b
Elise Phan <aliceconnic@gmail.com>
2019-01-12 04:10:14 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -10,17 +10,5 @@ class CreateArtists < ActiveRecord::Migration
   end
   
 
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
- 
-ActiveRecord::Base.connection.execute(sql)
-
 
 end
